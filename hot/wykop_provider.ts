22da585e602922b8d8f5c72829b9b101709f75be
tbielaszewski <tomekbielaszewski@gmail.com>
2020-01-03 01:00:50 +0100

    working wykop provider

--- hot/wykop_provider.ts
+++ hot/wykop_provider.ts
@@ -16,16 +16,39 @@ const getHotByPeriod = (page, period) => {
     });
 };
 
-const getHot24 = (page) => {
-    return getHotByPeriod(page, 24);
-};
+interface WykopResponse {
+    response: any,
+    page: number,
+    period: number
+}
+
+function getHot24(page: number): Promise<WykopResponse> {
+    return getHotByPeriod(page, 24)
+        .then(res => {
+            return {
+                response: res,
+                page: page,
+                period: 24
+            }
+        });
+}
 
 export const getHot = () => {
     return Promise.all(_.range(1, 11)
         .map(getHot24))
-        .then(res => _.flatMap(res)
-            .map(page => page["data"]))
-        .then(pages => _.flatMap(pages))
+        .then(res => _.flatMap<WykopResponse>(res)
+            .map(singlePage => {
+                singlePage.response = singlePage.response.data;
+                return singlePage;
+            }))
+        .then(res => res
+            .map<WykopResponse>(singleRes => singleRes.response
+                .map(entry => {
+                    entry.page = singleRes.page;
+                    entry.period = singleRes.period;
+                    return entry;
+                })))
+        .then(entries => _.flatMap(entries))
         .then(res => {
             console.log(JSON.stringify(res));
             return res;
