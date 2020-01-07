c262f41ee0774f74a1cf42aed272ac47efe08bfe
tbielaszewski <tomekbielaszewski@gmail.com>
2020-01-07 20:26:44 +0100

    changed dep for user_notifier in main handler

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const hotFetcher = require('./hot/fetcher');
-const userNotifier = require('./notifier/userNotifier');
+const userNotifier = require('./notifier/user_notifier');
 
 module.exports.hotFetcher = hotFetcher.fetch;
 module.exports.userNotifier = userNotifier.notify;
